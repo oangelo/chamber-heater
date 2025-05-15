67ae22a3aa79d7b744cc8dcab7ba1a71f2616226
ViniciusCMB <vinicius.bandeira@grad.iprj.uerj.br>
2025-05-15 09:30:36 -0300

    update arquivos

---
+++ firmware/firmware.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+// Definições de Pinos e Constantes
+
+
+// Declaração de Variáveis Globais
+
+
+// Funções Auxiliares
+
+
+// Função setup
+void setup() {
+  // Inicialização e configurações iniciais
+  Serial.begin(9600);
+  Serial.println("Projeto Arduino Iniciado!");
+}
+
+// Função loop
+void loop() {
+  // Lógica principal do seu projeto
+  delay(1000); // Exemplo de delay de 1 segundo
+}

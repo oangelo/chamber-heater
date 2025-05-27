cb22a3e0f6b7636f2263d79f68891d0917f75892
SerraRocketry <serra.rocketry@iprj.uerj.br>
2025-05-27 14:24:07 -0300

    add fan pid

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,27 +5,27 @@
 #include <Pushbutton.h>
 
 // Definições de pinos e constantes
-#define FAN_PIN 9      // Ventoinha
-#define INPUT_PIN 14   // Termistor mesa
-#define OUTPUT_PIN 8   // Mesa
-#define RT0 100000     // Ω
-#define B 3950         // K
-#define VCC 5          // Supply voltage
-#define R 4700         // R=10KΩ
-#define DHT_PIN 4      // Sensor temperatura
-#define DHT_TYPE DHT22 // Sensor temperatura
-#define LCD_COLS 20    // Colunas do LCD
-#define LCD_ROWS 4     // Linhas do LCD
-#define BTN_EN1 31     // Encoder 1
-#define BTN_EN2 33     // Encoder 2
-#define BTN_ENC 35     // Botão do encoder
-#define BUZZER_PIN 37  // Buzzer
-#define LCD_RS 16      // LCD RS Pin
-#define LCD_EN 17      // LCD EN Pin
-#define LCD_D4 23      // LCD D4 Pin
-#define LCD_D5 25      // LCD D5 Pin
-#define LCD_D6 27      // LCD D6 Pin
-#define LCD_D7 29      // LCD D7 Pin
+#define FAN_PIN 9        // Ventoinha
+#define bed_INPUT_PIN 14 // Termistor mesa
+#define bed_OUTPUT_PIN 8 // Mesa
+#define RT0 100000       // Ω
+#define B 3950           // K
+#define VCC 5            // Supply voltage
+#define R 4700           // R=10KΩ
+#define DHT_PIN 4        // Sensor temperatura
+#define DHT_TYPE DHT22   // Sensor temperatura
+#define LCD_COLS 20      // Colunas do LCD
+#define LCD_ROWS 4       // Linhas do LCD
+#define BTN_EN1 31       // Encoder 1
+#define BTN_EN2 33       // Encoder 2
+#define BTN_ENC 35       // Botão do encoder
+#define BUZZER_PIN 37    // Buzzer
+#define LCD_RS 16        // LCD RS Pin
+#define LCD_EN 17        // LCD EN Pin
+#define LCD_D4 23        // LCD D4 Pin
+#define LCD_D5 25        // LCD D5 Pin
+#define LCD_D6 27        // LCD D6 Pin
+#define LCD_D7 29        // LCD D7 Pin
 
 // Definições de variáveis
 int row[4] = {0, 1, 2, 3}; // Linhas do LCD
@@ -63,17 +63,19 @@ byte bitmapSeta2[8] =
         0b11000,
         0b10000};
 
-float internal_temp;                // Temperatura interna (DHT22)
-float internal_hum;                 // Umidade interna (DHT22)
-float bed_temp;                     // Temperatura da mesa (INPUT_PIN - Termistor)
-double Setpoint, Input, Output;     // Variáveis do PID da mesa
+float internal_temp;                         // Temperatura interna (DHT22)
+float internal_hum;                          // Umidade interna (DHT22)
+float bed_temp;                              // Temperatura da mesa (bed_INPUT_PIN - Termistor)
+double bed_set_point, bed_INPUT, bed_OUTPUT; // Variáveis do PID da mesa
+double fan_set_point, fan_INPUT, fan_OUTPUT;
 const int Kp = 15, Ki = 1, Kd = 15; // Constantes do PID da mesa
 
 RotaryEncoder encoder(BTN_EN1, BTN_EN2, RotaryEncoder::LatchMode::TWO03); // Encoder
 Pushbutton button(BTN_ENC);                                               // Botão do encoder
 DHT dht(DHT_PIN, DHT_TYPE);                                               // Sensor de temperatura e umidade
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);        // LCD
-PID bedPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);               // PID da mesa
+PID bedPID(&bed_INPUT, &bed_OUTPUT, &bed_set_point, Kp, Ki, Kd, DIRECT);  // PID da mesa
+PID fanPID(&fan_INPUT, &fan_OUTPUT, &fan_set_point, Kp, Ki, Kd, REVERSE); // PID da ventoinha
 
 // Retorna a temperatura da mesa
 float readBedTemp()
@@ -81,7 +83,7 @@ float readBedTemp()
   float RT, VR, ln, TX, VRT;
   const float T0 = 20 + 273.15; // K -> °C
 
-  VRT = analogRead(INPUT_PIN); // Lê o valor analógico do pino do termistor
+  VRT = analogRead(bed_INPUT_PIN); // Lê o valor analógico do pino do termistor
 
   VRT = (5.00 / 1023.00) * VRT; // Converte o valor lido para tensão
 
@@ -100,9 +102,17 @@ float readBedTemp()
 // Calcula o PID da mesa
 void calcBedPID()
 {
-  Input = readBedTemp();           // Lê a temperatura da mesa
-  bedPID.Compute();                // Calcula o PID
-  analogWrite(OUTPUT_PIN, Output); // Ajusta a potência da mesa
+  bed_INPUT = readBedTemp();               // Lê a temperatura da mesa
+  bedPID.Compute();                        // Calcula o PID
+  analogWrite(bed_OUTPUT_PIN, bed_OUTPUT); // Ajusta a potência da mesa
+}
+
+// Calcula o PID da ventoinha
+void calcFanPID()
+{
+  fan_INPUT = dht.readHumidity();   // Lê a umidade do DHT22
+  fanPID.Compute();                 // Calcula o PID
+  analogWrite(FAN_PIN, fan_OUTPUT); // Ajusta a potência da ventoinha
 }
 
 // Imprime os dados no Serial
@@ -119,12 +129,12 @@ void printInfo()
   Serial.print("Temperatura mesa: ");
   Serial.print(bed_temp);
   Serial.print(" °C");
-  Serial.print("   Setpoint: ");
-  Serial.print(Setpoint);
-  Serial.print(" °C");
-  Serial.print("   PWM: ");
-  Serial.print(map(Output, 0, 255, 0, 100));
-  Serial.println(" % ");
+  Serial.print("   PWM Mesa: ");
+  Serial.print(map(bed_OUTPUT, 0, 255, 0, 100));
+  Serial.print(" %");
+  Serial.print("   PWM Fan: ");
+  Serial.print(map(fan_OUTPUT, 0, 125, 0, 100));
+  Serial.println(" %");
 }
 
 // Função setup
@@ -135,15 +145,22 @@ void setup()
 
   dht.begin(); // Inicializa o sensor DHT
 
-  pinMode(BTN_ENC, INPUT);
-  pinMode(OUTPUT_PIN, OUTPUT);
-  pinMode(FAN_PIN, OUTPUT);
-  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BTN_ENC, bed_INPUT);
+  pinMode(bed_OUTPUT_PIN, bed_OUTPUT);
+  pinMode(FAN_PIN, bed_OUTPUT);
+  pinMode(BUZZER_PIN, bed_OUTPUT);
 
-  Input = readBedTemp(); // Lê a temperatura inicial da mesa
-  Setpoint = 100;        // Define a temperatura desejada (Setpoint)
+  bed_INPUT = readBedTemp(); // Lê a temperatura inicial da mesa
+  bed_set_point = 130;       // Define a temperatura desejada (bed_set_point)
 
   bedPID.SetMode(AUTOMATIC); // Ativa o modo automático do PID
+
+  float Umi = dht.readHumidity();
+  fan_INPUT = Umi;
+  fan_set_point = 10;
+
+  fanPID.SetMode(AUTOMATIC);
+  fanPID.SetOutputLimits(0, 125);
 
   digitalWrite(BUZZER_PIN, HIGH);
   delay(200);
@@ -157,5 +174,6 @@ void setup()
 void loop()
 {
   calcBedPID();
+  calcFanPID();
   printInfo();
 }
--- test/fan_pid/fan_pid.ino
+++ test/fan_pid/fan_pid.ino
@@ -7,11 +7,11 @@
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
-double Umi_set, Umidade, PWM;
+double fan_set_point, fan_INPUT, fan_OUTPUT;
 
 const int Kp = 15, Ki = 1, Kd = 15;
 
-PID myPID(&Umidade, &PWM, &Umi_set, Kp, Ki, Kd, REVERSE);
+PID fanPID(&fan_INPUT, &fan_OUTPUT, &fan_set_point, Kp, Ki, Kd, REVERSE);
 
 void setup()
 {
@@ -19,23 +19,23 @@ void setup()
   Serial.begin(9600);
   Serial.println("Iniciando");
   float Umi = dht.readHumidity();
-  Umidade = Umi;
-  Umi_set = 10;
+  fan_INPUT = Umi;
+  fan_set_point = 10;
 
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetOutputLimits(0,255);
+  fanPID.SetMode(AUTOMATIC);
+  fanPID.SetOutputLimits(0,255);
 }
 
 void loop()
 {
   float Umi = dht.readHumidity();
-  Umidade = Umi;
-  myPID.Compute();
-  analogWrite(FAN_PIN, PWM);
-  Serial.print("Umidade: ");
+  fan_INPUT = Umi;
+  fanPID.Compute();
+  analogWrite(FAN_PIN, fan_OUTPUT);
+  Serial.print("fan_INPUT: ");
   Serial.print(Umi);
   Serial.print(" | PID: ");
-  Serial.print(map(PWM, 0, 255, 0, 100));
+  Serial.print(map(fan_OUTPUT, 0, 255, 0, 100));
   Serial.print(" | Output: ");
-  Serial.println(PWM);
+  Serial.println(fan_OUTPUT);
 }
